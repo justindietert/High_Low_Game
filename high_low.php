a2b254d2f05715cd673e8484f29a650241e5f9ab
Justin Dietert <justin.dietert@gmail.com>
2015-03-10 16:17:53 -0500

    added CLI arguments feature to game

--- high_low.php
+++ high_low.php
@@ -1,29 +1,34 @@
 <?php
 
-do {
-    $random = rand(1, 100);
-
+if ($argc == 3) {
     do {
-        fwrite(STDOUT, 'What number am I thinking of from 1 to 100? ');
-        $guess = trim(fgets(STDIN));
+        $random = rand($argv[1], $argv[2]);
 
-        if (!is_numeric($guess)) {
-                fwrite(STDOUT, "$guess is not a number. Guess a numeral from 1 to 100 only.\n");
-            } elseif ($guess < 1 || $guess > 100) {
-                fwrite(STDOUT, "$guess is out of range. Guess a numeral from 1 to 100 only.\n");
-            } elseif ($guess < $random) {
-                fwrite(STDOUT, "$guess is wrong. Guess HIGHER.\n");
-            } elseif ($guess > $random) {
-                fwrite(STDOUT, "$guess is wrong. Guess LOWER.\n");
-            } else {
-                fwrite(STDOUT, "Correct! My number is $guess!\n");
-                fwrite(STDOUT, "Enter 'y' to play again. Enter any other character to exit. ");
-                $answer = trim(fgets(STDIN));
-                if ($answer != "y") {
-                    exit(0);
+        do {
+            fwrite(STDOUT, "What number am I thinking of from $argv[1] to $argv[2]? ");
+            $guess = trim(fgets(STDIN));
+
+            if (!is_numeric($guess)) {
+                    fwrite(STDOUT, "$guess is not a number. Guess a numeral from $argv[1] to $argv[2] only.\n");
+                } elseif ($guess < $argv[1] || $guess > $argv[2]) {
+                    fwrite(STDOUT, "$guess is out of range. Guess a numeral from $argv[1] to $argv[2] only.\n");
+                } elseif ($guess < $random) {
+                    fwrite(STDOUT, "$guess is wrong. Guess HIGHER.\n");
+                } elseif ($guess > $random) {
+                    fwrite(STDOUT, "$guess is wrong. Guess LOWER.\n");
+                } else {
+                    fwrite(STDOUT, "Correct! My number is $guess!\n");
+                    fwrite(STDOUT, "Enter 'y' to play again. Enter any other character to exit. ");
+                    $answer = trim(fgets(STDIN));
+                    if ($answer != "y") {
+                        exit(0);
+                    }
                 }
-            }
 
-    } while ($guess != $random);
+        } while ($guess != $random);
 
-} while ($answer == "y");
+    } while ($answer == "y");
+
+} else {
+    echo "Enter 2 arguments only.";
+}
