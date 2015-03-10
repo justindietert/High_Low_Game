c4d117c3ada61103f385004429b26db34e1e20fc
Justin Dietert <justin.dietert@gmail.com>
2015-03-10 15:51:02 -0500

    completed initial version of high_low guessing game

--- high_low.php
+++ high_low.php
@@ -1,2 +1,29 @@
 <?php
 
+do {
+    $random = rand(1, 100);
+
+    do {
+        fwrite(STDOUT, 'What number am I thinking of from 1 to 100? ');
+        $guess = trim(fgets(STDIN));
+
+        if (!is_numeric($guess)) {
+                fwrite(STDOUT, "$guess is not a number. Guess a numeral from 1 to 100 only.\n");
+            } elseif ($guess < 1 || $guess > 100) {
+                fwrite(STDOUT, "$guess is out of range. Guess a numeral from 1 to 100 only.\n");
+            } elseif ($guess < $random) {
+                fwrite(STDOUT, "$guess is wrong. Guess HIGHER.\n");
+            } elseif ($guess > $random) {
+                fwrite(STDOUT, "$guess is wrong. Guess LOWER.\n");
+            } else {
+                fwrite(STDOUT, "Correct! My number is $guess!\n");
+                fwrite(STDOUT, "Enter 'y' to play again. Enter any other character to exit. ");
+                $answer = trim(fgets(STDIN));
+                if ($answer != "y") {
+                    exit(0);
+                }
+            }
+
+    } while ($guess != $random);
+
+} while ($answer == "y");
